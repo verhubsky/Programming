e9e16aa0a1bb1961712f1f4e435e89914ce02dbf
Fedor <feda.vert@gmail.com>
2026-03-18 11:30:22 +0300

    Add Karnaugh map functionality in karnaugh.cpp
    Implement Karnaugh map printing for logical expressions.

---
+++ practise/karnaugh.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+void printHeader() {
+    cout << " x\\yz | 00 | 01 | 11 | 10 |\n";
+    cout << "------|----|----|----|----|\n";
+}
+int main() {
+    cout << "Карта для: (x && y) || z\n";
+    printHeader();
+    for (int x : {0, 1}) {
+        cout << "  " << x << "   | " 
+             << ((x&&0)||0) << "  | " << ((x&&0)||1) << "  | " 
+             << ((x&&1)||1) << "  | " << ((x&&1)||0) << "  |\n";
+    }
+    cout << "\nКарта для: x  !y  !z\n";
+    printHeader();
+    for (int x : {0, 1}) {
+        cout << "  " << x << "   | " 
+             << (x||!0||!0) << "  | " << (x||!0||!1) << "  | " 
+             << (x||!1||!1) << "  | " << (x||!1||!0) << "  |\n";
+    }
+    cout << "\nКарта для: !x  y  z\n";
+    printHeader();
+    for (int x : {0, 1}) {
+        cout << "  " << x << "   | " 
+             << (!x||0||0) << "  | " << (!x||0||1) << "  | " 
+             << (!x||1||1) << "  | " << (!x||1||0) << "  |\n";
+    }
+    return 0;
+}
